d7cf6d139c0a27537ba7c76509327014c3135d48
ejang1 <ejang1@binghamton.edu>
2021-09-30 17:18:49 -0400

    monster face building

--- click_hunt/lib/monster.dart
+++ click_hunt/lib/monster.dart
@@ -10,9 +10,15 @@ class Monster extends CustomPainter{
     var paint1 = Paint()
       ..color = mcolor
       ..style = PaintingStyle.fill;
+    var paint2 = Paint()
+      ..color = Colors.black
+      ..style = PaintingStyle.fill;
     canvas.drawCircle(Offset(0,0), csize, paint1);
+    canvas.drawRect(Rect.fromCenter(center: Offset(0+csize/2,0-csize/2), width: csize*0.8, height: csize*0.8), paint1);
+    canvas.drawRect(Rect.fromCenter(center: Offset(0-csize/2,0-csize/2), width: csize*0.8, height: csize*0.8), paint1);
+    canvas.drawCircle(Offset(0+csize/3,0-csize/3), 2+csize/10, paint2);
+    canvas.drawCircle(Offset(0-csize/3,0-csize/3), 2+csize/10, paint2);
   }
-
   @override
   bool shouldRepaint(covariant CustomPainter oldDelegate) => true;
 }
