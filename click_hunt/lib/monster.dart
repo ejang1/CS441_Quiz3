0f80124cdf6d9ef57ffc565b43b6993f9a0ede7a
ejang1 <ejang1@binghamton.edu>
2021-09-30 18:07:15 -0400

    monster drawing over

--- click_hunt/lib/monster.dart
+++ click_hunt/lib/monster.dart
@@ -13,11 +13,25 @@ class Monster extends CustomPainter{
     var paint2 = Paint()
       ..color = Colors.black
       ..style = PaintingStyle.fill;
+    var paint3 = Paint()
+      ..color = Colors.black
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = csize*0.1
+      ..strokeCap = StrokeCap.round;
     canvas.drawCircle(Offset(0,0), csize, paint1);
     canvas.drawRect(Rect.fromCenter(center: Offset(0+csize/2,0-csize/2), width: csize*0.8, height: csize*0.8), paint1);
     canvas.drawRect(Rect.fromCenter(center: Offset(0-csize/2,0-csize/2), width: csize*0.8, height: csize*0.8), paint1);
     canvas.drawCircle(Offset(0+csize/3,0-csize/3), 2+csize/10, paint2);
     canvas.drawCircle(Offset(0-csize/3,0-csize/3), 2+csize/10, paint2);
+
+    Path path = Path();
+    path.moveTo(size.width/2-csize/3,size.height/2+csize/2);
+    //path.quadraticBezierTo(0, csize*0.2, size.width/2+csize/3, size.height/2+csize/2);
+     path.lineTo(0,csize*0.2);
+     path.lineTo(size.width/2+csize/3,size.height/2+csize/2);
+    //path.close();
+    canvas.drawPath(path, paint3);
+
   }
   @override
   bool shouldRepaint(covariant CustomPainter oldDelegate) => true;
